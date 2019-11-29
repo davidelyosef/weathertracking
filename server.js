0f7af5b5a0c5b9f7ae063ac7c05d189e9fed3c83
David el Yosef <davidyf96@gmail.com>
2019-11-29 11:34:42 +0200

    another shot

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/dist"));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => console.log("Listening..."));
 
 app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-console.log("Listening...");
+
 
