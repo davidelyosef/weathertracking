da7821c695ed0498b72894f6f764f6f907284675
David el Yosef <davidyf96@gmail.com>
2019-11-29 19:08:03 +0200

    refreshing

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -10,14 +10,14 @@ export class WeatherService {
   constructor(private httpClient: HttpClient) { }
 
   public getLocationKey(city): any {
-    return this.httpClient.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=Bdtfu2OnDFSAmpquELvzARuxy4FlE2E1&q=${city}`);
+    return this.httpClient.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=Bdtfu2OnDFSAmpquELvzARuxy4FlE2E1&q=${city}`);
   }
 
   public getCityDetails(location_key): any {
-    return this.httpClient.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${location_key}?apikey=Bdtfu2OnDFSAmpquELvzARuxy4FlE2E1`);
+    return this.httpClient.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${location_key}?apikey=Bdtfu2OnDFSAmpquELvzARuxy4FlE2E1`);
   }
 
   public getGeolocation(string): any {
-    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/forecast${string}&APPID=e4063054bda92f3ca54619eb59a22adf`)
+    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/forecast${string}&APPID=e4063054bda92f3ca54619eb59a22adf`)
   }
 }
